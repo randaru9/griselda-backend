aa3caaf7bb9b6e3a860d31c3e749390ee91edb27
randaru09 <ndaruanggoro123@gmail.com>
2023-12-30 17:04:30 +0700

    setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+import { TypeOrmConfig } from './utils/TypeOrmConfig';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { Configuration } from './utils/Configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [Configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfig,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/Configuration.ts
@@ -0,0 +1,28 @@
+// import { ExtractJwt } from 'passport-jwt';
+
+export const Configuration = () => ({
+	database: {
+		port: process.env.DB_PORT,
+		type: process.env.DB_TYPE,
+		host: process.env.DB_HOST,
+
+		password: process.env.DB_PASSWORD,
+		username: process.env.DB_USERNAME,
+		database: process.env.DB_DATABASE,
+		
+		synchronize: process.env.DB_SYNC,
+		entities: [__dirname + '/../**/*.entity.{ts,js}'],
+	},
+	// jwt: {
+	// 	global: process.env.JWT_GLOBAL,
+	// 	secret: process.env.JWT_SECRET,
+	// 	signOptions: {
+	// 		expiresIn: process.env.JWT_EXPIRES_IN,
+	// 	},
+	// },
+	// jwtStrategy: {
+	// 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+	// 	ignoreExpiration: process.env.JWT_STRATEGY_IGNORE_EXPIRATION,
+	// 	secretOrKey: process.env.JWT_SECRET,
+	// }
+})
